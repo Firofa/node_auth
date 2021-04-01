b76047dab11938c1d38bc3cee7894a895c9c7333
Firofa <rizifaarsien@gmail.com>
2021-04-01 15:25:57 +0700

    Register API Post masih kosong

--- routes/routes.js
+++ routes/routes.js
@@ -3,16 +3,16 @@ const bcrypt = require('bcryptjs')
 const User = require('../models/user')
 
 
-router.post('/register', async (req,res) => {
-    const salt = await bcrypt.genSaltSync(10)
-    const hashedPassword = await bcrypt.hashSync(req.body.password, salt)
+router.post('/register', (req,res) => {
+    const salt = bcrypt.genSaltSync(10)
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt)
     const user = new User({
         name: req.body.name,
         email: req.body.email,
         password: hashedPassword
     })
     
-    res.send(await user.save())
+    res.send(user.save())
 })
 
 module.exports = router;
